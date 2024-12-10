f4a97b606491654766bea73892a04a01c382f0dc
Malvina <malvinapalmero.dev@gmail.com>
2024-12-10 20:36:28 -0300

    Corrección de errores

--- script.js
+++ script.js
@@ -18,7 +18,28 @@ botonDescansoLargo.addEventListener('click', () =>{
 })
 
 
-function cambiarContexto (contexto){
-    html.setAttribute('data-contexto', "contexto");
-    banner.setAttribute('src', `./imagenes/${contexto}.png`);
+function cambiarContexto(contexto) {
+    
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagenes/${contexto}.png`);
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `
+            Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>
+            `;
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `
+            ¿Qué tal tomar un respiro? <strong class="app__title-strong">¡Haz una pausa corta!</strong>
+            `;
+            break;
+        case "descanso-largo":
+            titulo.innerHTML = `
+            Hora de volver a la superficie.<strong class="app__title-strong"> Haz una pausa larga.</strong>
+            `;
+            break;
+        default:
+            break;
+    }
 }
