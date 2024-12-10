f50922ffbef423dcc388b0643ed115891ed1f9fe
Malvina <malvinapalmero.dev@gmail.com>
2024-12-10 20:00:26 -0300

    Trabajando con atributos

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const html = document.querySelector('html');
 const botonDescansoCorto = document.querySelector('.app__card-button--corto');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonDescansoLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 botonDescansoCorto.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'descanso-corto');
+    cambiarContexto ('descanso-corto');
 })
 
 botonEnfoque.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'enfoque');
+    cambiarContexto ('enfoque'); 
 })
 
 botonDescansoLargo.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', "descanso-largo");
+    cambiarContexto ('descanso-largo')
 })
+
+
+function cambiarContexto (contexto){
+    html.setAttribute('data-contexto', "contexto");
+    banner.setAttribute('src', `./imagenes/${contexto}.png`);
+}
